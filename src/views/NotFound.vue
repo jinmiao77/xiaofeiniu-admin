087d27df24d1f19f5c8f36a1f33120eda2042812
jinmiao <564703603@qq.com>
2019-01-31 20:19:42 +0800

    submit

---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="xfn-notFound">
+    <h1>404 Not Found!</h1>
+  </div>
+</template>
+<style lang="scss">
+  .xfn-notFound{
+    text-align: center;
+    padding-top:200px;
+  }
+</style>
